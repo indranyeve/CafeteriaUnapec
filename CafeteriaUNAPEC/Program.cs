34f382daf1ca4a530e4b75fdd5a296746547485d
indranyeve <indranyeve@gmail.com>
2020-08-16 23:33:11 -0400

    12.32PM

--- CafeteriaUNAPEC/Program.cs
+++ CafeteriaUNAPEC/Program.cs
@@ -16,7 +16,7 @@ namespace CafeteriaUNAPEC
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new GestionCafeteria());
+            Application.Run(new GestionArticulos());
         }
     }
 }
